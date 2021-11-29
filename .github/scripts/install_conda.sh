8375df6552b8db8172ec129b4466435ed985c767
Tom Yip <tom.yip@oracle.com>
2021-11-29 14:53:13 -0800

    Replace the bash shell path according to this: https://github.com/nextflow-io/nextflow/pull/2341

--- .github/scripts/install_conda.sh
+++ .github/scripts/install_conda.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eo pipefail
 
 echo Install Conda .. >> artifacts/test_artifact.log
@@ -8,7 +8,7 @@ sudo apt-get update --fix-missing && sudo apt-get install -y wget bzip2 ca-certi
     libglib2.0-0 libxext6 libsm6 libxrender1 \
     git mercurial subversion
 wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
-    /bin/bash ~/miniconda.sh -b -p /opt/conda && \
+    /usr/bin/env bash ~/miniconda.sh -b -p /opt/conda && \
     rm ~/miniconda.sh && \
     ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
     echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
--- .github/scripts/test_cram_input.sh
+++ .github/scripts/test_cram_input.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eo pipefail
 export PATH=/opt/conda/bin:$PATH
 
